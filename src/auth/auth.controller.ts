195ae4245d2d4ee66213ee37335b85818e8f8f6b
GuariosMaster <guariosdeveloper@gmail.com>
2024-03-02 22:37:21 -0500

    funt: Authorization the at api

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Get, Request, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "./guard/auth/auth.guard";
+import { LoginDto } from "./dto/login.dto";
+import { RegisterDto } from "./dto/register.dto";
+import { AuthService } from "./auth.service";
+import { Roles } from "./decorators/roles.decorator";
+import { RolesGuard } from "./guard/roles.guard";
+import { Role } from "./enums/role.enum";
+import { Auth } from "./decorators/auth.decorators";
+
+interface RequestWithUser extends Request {
+  user: { email: string; role: string };
+}
+
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+  
+    @Post("register")
+    register(@Body() registerDto: RegisterDto) {
+      return this.authService.register(registerDto);
+    }
+  
+    @HttpCode(HttpStatus.OK)
+    @Post("login")
+    login(@Body() loginDto: LoginDto) {
+      return this.authService.login(loginDto);
+    }
+
+    @Get('profile')
+    @Auth(Role.ADMIN)
+    profile(@Request() req: RequestWithUser) {
+      return this.authService.profile(req.user);
+    }
+}
